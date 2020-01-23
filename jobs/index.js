09b0e61e69f79599ba76330e8e73fdddeb5afeec
Ernest Hayford <ernest.hayford@bluecrest.edu.gh>
2020-01-23 15:42:22 +0000

    fixed returnd links for getCondition

--- jobs/index.js
+++ jobs/index.js
@@ -1,15 +1,15 @@
 const mayoclinic = require('./mayoclinic')
 const fs = require('fs')
 
-try{
-    let conditions = await mayoclinic.init()
-    await fs.writeFile('./bin/conditions.json', conditions, 'utf8', function(error){
+const startJob = async function (){
+    let conditions = await mayoclinic()
+    await fs.writeFile('./bin/conditions.json', JSON.stringify(conditions), 'utf8', function(error){
         if(error){
             console.log("An error occured while writing JSON Object to File.");
             return err
         }
         console.log("JSON file has been saved.")
     })
-}catch(e){
-    console.error(e)
 }
+
+startJob()
